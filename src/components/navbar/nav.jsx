40d0559b55a9fccc7ab6acfbe9e2fa5db9d7cf82
James Jelo <jamesjelo@Jamess-MacBook-Pro.local>
2023-10-31 20:23:53 +0100

    Made the rightmost button of the navbar dynamic

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,9 +1,20 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
 import styles from './nav.module.css';
+import { usePathname } from 'next/navigation';
 
 const NavBar = () => {
-    
+
+  const pathname = usePathname();
+
+  const paths = {
+    "/": "Get Started",
+    "/createWallet": "Create Wallet"
+  }
+
+  const btnText = paths[pathname] ?? "Connect Wallet"
+
   return (
     <nav className={styles.NavBar}>
         <Link className={styles.logo} href="/">WalletSplit</Link>
@@ -11,7 +22,7 @@ const NavBar = () => {
             <p>How it works</p>
         </div>
         <div className={styles.auth}>
-            <button>Get Started</button>
+            <button>{btnText}</button>
         </div>
     </nav>
   )
