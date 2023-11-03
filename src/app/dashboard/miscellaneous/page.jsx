34f90be0a8cc3273c9554fc295b0ca2461b25946
James Jelo <jamesjelo@Jamess-MacBook-Pro.local>
2023-11-03 20:47:29 +0100

    Done with mobile styling for Miscellaneous page

--- src/app/dashboard/miscellaneous/page.jsx
+++ src/app/dashboard/miscellaneous/page.jsx
@@ -11,28 +11,28 @@ const Miscellaneous = () => {
   return (
     <div className={styles.miscellaneous}>
         <p className={styles.title}>Miscellaneous</p>
-        <p>You can send money to any address not in your family and friends address book</p>
+        <p className={styles.subTitle}>You can send money to any address not in your family and friends address book</p>
         <section className={styles.miscBalanceContainer}>
             <p className={styles.miscWalletLabel}>Miscellaneous Balance</p>
             <p className={styles.miscWalletValue}>{allocations.miscellaneous}<span>ETH</span></p>
             <div className={styles.actions}>
-                <div>
+                <div className={styles.actionsBox}>
                     <Image 
                         src="/send.svg"
                         width={20}
                         height={20}
                         alt="Send"
                     /> 
-                    <p>Send</p>
+                    <p className={styles.actionLabel}>Send</p>
                 </div>
-                <div>
+                <div className={styles.actionsBox}>
                     <Image 
                         src="/update.svg"
                         width={20}
                         height={20}
                         alt="Update"
                     /> 
-                    <p>Update</p>
+                    <p className={styles.actionLabel}>Update</p>
                 </div>
             </div>
         </section>
